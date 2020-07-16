278acc7052ce450f1e7e4f848b39666b8d1369ea
nereid-n <nas.kuleshova@gmail.com>
2020-07-16 18:20:32 +0300

    add flip on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './components/card';
+import { Cards } from './store/cards';
 import './App.scss';
 
-interface Cards {
-  img: string
-}
-
-const cards: Array<Cards> = [
-  {
-    img: 'circle'
-  },
-  {
-    img: 'square'
-  },
-  {
-    img: 'flower'
-  },
-  {
-    img: 'triangle'
-  },
-];
-
-function onClick(event: React.MouseEvent) {
-  console.log(event);
-}
-
+let countOpenCards: number = 0;
 function App() {
+  const [cards, setCards] = useState([
+    {
+      img: 'circle',
+      status: 'g'
+    },
+    {
+      img: 'square',
+      status: ''
+    },
+    {
+      img: 'flower',
+      status: ''
+    },
+    {
+      img: 'triangle',
+      status: ''
+    },
+  ]);
+  const [wrapClassName, setWrapClassName] = useState('cards');
+
+  const onClick = (index: number) => {
+    countOpenCards++;
+    const newCards: Array<Cards> = [...cards];
+    newCards[index].status = 'open';
+    setCards(newCards);
+    if(countOpenCards === 2) {
+      setWrapClassName(prevState => prevState + ' no-click');
+      setTimeout(() => {
+        newCards.map((card) => {
+          card.status = '';
+          return card;
+        });
+        setCards(newCards);
+        setWrapClassName((prevState) => prevState.replace(' no-click', ''));
+        countOpenCards = 0;
+      }, 2000);
+    }
+  }
+
   return (
     <div className="App">
       <div className="container">
-        <div className="cards">
+        <div className={wrapClassName}>
           {cards.map((card, index) => <Card
             data={card}
+            index={index}
             key={index}
-            onClick={(event) => onClick(event)}/>
+            onClick={(index) => onClick(index)}/>
           )}
         </div>
       </div>
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactSVG } from 'react-svg';
 import './card.scss';
 
 interface StandardComponentProps {
   data: {
-    img: string
+    img: string,
+    status: string
   },
-  onClick: (event: React.MouseEvent<HTMLDivElement>) => void
+  index: number,
+  onClick: (index: number) => void
 }
 
 function Card(props: StandardComponentProps) {
+
+  const [wrapClassName, setWrapClassName] = useState('card-wrap');
+
+  useEffect(() => {
+    if (props.data.status === 'open') {
+      setWrapClassName((prevState) => prevState + ' card-open');
+    } else {
+      setWrapClassName((prevState) => prevState.replace(' card-open', ''));
+    }
+  }, [props.data.status]);
+
   return (
     <div
-      className="card-wrap"
-      onClick={(event) => {props.onClick(event)}}
+      className={wrapClassName}
+      onClick={() => {props.onClick(props.index)}}
     >
-      <div className="card-top card"></div>
-      <div className="card-inner card">
+      <div className="card-front card"></div>
+      <div className="card-back card">
         <ReactSVG
           src={`img/cards/${props.data.img}.svg`}
           className="card__svg"
